9db48dbdf37877848255241ac0b8886e54d98b8e
JustGetSchwifty <tomas@manena.cz>
2022-08-18 11:55:38 +0200

    1 binaries uploaded

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const authorize = (req, res, next) => {
+  const auth = { login: 'test', password: 'test' }; // change this
+
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = Buffer.from(b64auth, 'base64')
+    .toString()
+    .split(':');
+
+  if (login && password && login === auth.login && password === auth.password) {
+    return next();
+  }
+
+  res.set('WWW-Authenticate', 'Basic realm="401"');
+  res.status(401).send('Authentication required.');
+};
+
+app.use('/', authorize);
+app.use('/', express.static('build'));
+
+app.listen(3000);
