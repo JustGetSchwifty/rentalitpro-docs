c48a6a2b72ad7b90d745d5436da2a7d26fedc26d
JustGetSchwifty <tomas@manena.cz>
2022-08-22 22:36:10 +0200

    1 file modified, 1 added, 1 deleted

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'static/favicon.ico',
+  favicon: '/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
